caa48409cbf6871e4e08e8cd9d7c141505e22001
Selena <selena.poun@gmail.com>
2021-11-24 12:17:56 +0100

    update

--- my-app/src/Components/Games.jsx
+++ my-app/src/Components/Games.jsx
@@ -1,27 +1,7 @@
 import React from "react";
-import { useParams } from "react-router";
-import { useEffect, useState } from "react";
-import axios from "axios";
 
-const Games = () => {
-  const { id } = useParams();
-  const [game, setGames] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get(`https://wild-games.jsrover.wilders.dev/games/${id}`)
-      .then((res) => {
-        setGames(res.data);
-      });
-  }, [id]);
-
-  if (!Games) {
-    return (
-      <div>
-        <span>LOADING</span>
-      </div>
-    );
-  }
-};
+function Games() {
+  return <></>;
+}
 
 export default Games;
