68b8f7f8771161ce03e1a727533b02abc9111f16
Selena <selena.poun@gmail.com>
2021-11-24 10:18:19 +0100

    components games and games list ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Games from "./Components/Games";
+import GamesList from "./Components/GamesList";
 
 function App() {
-  return <Header title="Ben" />;
+  return (
+    <>
+      <Header title="Ben" />;
+      <Games />;
+      <GamesList />;
+    </>
+  );
 }
 
 export default App;
