ea3f438778af330f0b1ff150e305f3b6c9d324d9
Selena <selena.poun@gmail.com>
2021-11-24 12:27:14 +0100

    add link

--- my-app/src/Components/Games.jsx
+++ my-app/src/Components/Games.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Games() {
-  return <></>;
+function Games({ id, name, rating }) {
+  return (
+    <div>
+      <h1>
+        {" "}
+        <Link to={`/${id}`}>{name} </Link>{" "}
+      </h1>
+      <p>
+        <span>Rating: {rating}</span>
+      </p>
+    </div>
+  );
 }
 
 export default Games;
