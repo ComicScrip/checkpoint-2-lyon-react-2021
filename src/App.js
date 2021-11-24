5b91e6008a623f691b81deacf33269b90f65239d
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-11-24 10:06:37 +0100

    GamList ok with API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
+import Game from "./Components/Game";
+import GameList from "./Components/GameList";
 import Header from "./Components/Header";
 
 function App() {
-  return <Header name="Toto" />;
+  return (
+    <>
+      <Header name="Toto" />
+      <GameList />
+      <Game />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/GameList.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const GameList = () => {
+  const [gameList, setGameList] = useState("");
+
+  useEffect(() => {
+    axios.get("https://wild-games.jsrover.wilders.dev/games").then((res) => {
+      setGameList(res.data);
+    });
+  }, []);
+
+  if (!gameList) {
+    return (
+      <div>
+        <span>LOADING</span>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <ul>
+        {gameList.map(({ id, name }) => (
+          <li key={id}>({name})</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GameList;
