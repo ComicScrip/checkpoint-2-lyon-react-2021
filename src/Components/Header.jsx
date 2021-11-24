5a18ff157ceea4320f72c548fe41182c6690c804
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-11-24 13:03:07 +0100

    just start css, just very ugly

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./css/Header.css";
 
 const Header = ({ name }) => {
   return (
-    <>
+    <div id="header-container">
       <Link to="/">
-        <h1>{name}, welcome to this fun site !!!!</h1>
+        <h1 id="header-title">{name}, welcome to this fun site !!!!</h1>
       </Link>
-    </>
+    </div>
   );
 };
 
