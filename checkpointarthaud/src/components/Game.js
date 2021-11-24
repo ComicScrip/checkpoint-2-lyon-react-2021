db8311eec1fc25cccc0d4415da6b44ec5357fb98
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-11-24 10:50:56 +0100

    Add the API in my web site and add the css

--- checkpointarthaud/src/components/Game.js
+++ checkpointarthaud/src/components/Game.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const Game = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Game = ({game}) => {
+  return (
+    <>
+    <div key={game.id}>
+      <img
+        width="700"
+        height="500"
+        src={game.background_image}
+        alt="game poster"
+      />
+      <h3>{game.name}</h3>
+      <p>{game.released}</p>
+      <p>{game.slug}</p>
+      <ul>
+          {game.genres.map((genre) => (
+            <li>{genre.name}</li>
+          ))}
+        </ul>
+    </div>
+    </>
+  );
 };
 
 export default Game;
--- checkpointarthaud/src/components/GameList.js
+++ checkpointarthaud/src/components/GameList.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useState } from "react";
+import Game from "./Game";
 
 const GameList = () => {
   const [gameList, setGameList] = useState([]);
+  const [onlyTheBestGame, setOnlyTheBestGame] = useState(false);
+  function handleOnlyTheBestGame (){
+    setOnlyTheBestGame(!onlyTheBestGame)
+  } 
+
 
   useEffect(() => {
     axios
@@ -14,9 +20,16 @@ const GameList = () => {
       });
   }, []);
 
-  return <div>
-      
-  </div>;
-};
+  return (
+  
+  <div className='container'>
+      {gameList
+      .map((game) => (
+          <div key={game.id}>
+              <Game game={game} />
+      </div>
+      ))}
+  </div>
+  )};
 
 export default GameList;
