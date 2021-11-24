a8c421d85c7a2c628146daed8c32cbe3604dc297
bahstiien <bastien.lecalvez@gmail.com>
2021-11-24 09:40:25 +0100

    add the header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header title="Amazing Wild Game App" />
+    </div>
+  );
 }
 
 export default App;
