c7b60089350ab1cc933ef0aebd1458c7b6c92994
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-11-24 10:03:55 +0100

    Add Header and title site

---
+++ checkpointarthaud/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = ({MyWebSiteTitle}) => {
+    return (
+        <div>
+            <h1>Welcome to my {MyWebSiteTitle}</h1>
+        </div>
+    );
+};
+
+export default Header;
