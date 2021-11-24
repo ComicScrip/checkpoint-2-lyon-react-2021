4f1df6427089f08b64c7da231d4ed891e2a002a4
Yannis <yannis.barba.dev@gmail.com>
2021-11-24 10:50:36 +0100

    fetch the data from API in GameDetails for only one Game

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 function GameDetails() {
   const { id } = useParams();
-  return <div>GameDetails de {id}</div>;
+  const [game, setGame] = useState();
+
+  const getGame = async () => {
+    const res = await axios.get(
+      `https://wild-games.jsrover.wilders.dev/games/${id}`
+    );
+    setGame(res.data);
+  };
+
+  useEffect(() => {
+    getGame();
+  }, []);
+
+  if (!game) {
+    return <h1>Loading .. </h1>;
+  }
+
+  return <div>GameDetails de {game.name}</div>;
 }
 
 export default GameDetails;
