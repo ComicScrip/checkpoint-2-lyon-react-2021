1b8ae1e64fbfbb679391c8777c435e72b4b6aabe
solene Verrier <sosso711@hotmail.com>
2021-11-24 10:10:26 +0100

    create route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import GameList from "./components/GameList";
 import Header from "./components/Header";
 import Game from "./components/Game";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Header nameTitle=" wild game" />
-      <GameList />
-      <Game />
+      <BrowserRouter>
+        <Header nameTitle=" wild game" />
+        <Switch>
+          <Route exact path="/" component={GameList} />
+          <Route exact path="/:id" component={Game} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
