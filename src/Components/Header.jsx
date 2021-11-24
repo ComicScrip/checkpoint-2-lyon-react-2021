da8582553fe8c34eb2112130bbeca9de44088ac7
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-11-24 12:28:14 +0100

    all fonctional without options, no css

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = ({ name }) => {
-  return <h1>{name}, welcome to this fun site !!!!</h1>;
+  return (
+    <>
+      <Link to="/">
+        <h1>{name}, welcome to this fun site !!!!</h1>
+      </Link>
+    </>
+  );
 };
 
 export default Header;
